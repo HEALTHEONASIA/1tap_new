75344f47c264cd883bad2434e3fb4ab8c1767576
vesperalwall860 <vesperalwall860@gmail.com>
2017-04-22 15:39:32 +0300

    Added test chat

--- project/main/views.py
+++ project/main/views.py
@@ -21,6 +21,39 @@ from ..models import Claim, Member, Terminal, ICDCode, Provider, Payer
 from ..models import Doctor, User, GuaranteeOfPayment
 from ..models import monthdelta, login_required
 
+
+from gevent import monkey; monkey.patch_all()
+
+from socketio import socketio_manage
+from socketio.namespace import BaseNamespace
+from socketio.mixins import RoomsMixin, BroadcastMixin
+
+# The socket.io namespace
+class ChatNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
+    def on_nickname(self, nickname):
+        self.environ.setdefault('nicknames', []).append(nickname)
+        self.socket.session['nickname'] = nickname
+        self.broadcast_event('announcement', '%s has connected' % nickname)
+        self.broadcast_event('nicknames', self.environ['nicknames'])
+        # Just have them join a default-named room
+        self.join('main_room')
+
+    def on_user_message(self, msg):
+        self.emit_to_room('main_room', 'msg_to_room', self.socket.session['nickname'], msg)
+
+    def recv_message(self, message):
+        print "PING!!!", message
+
+
+@main.route('/chat')
+def index():
+    return render_template('chat.html')
+
+@main.route("/socket.io/<path:path>")
+def run_socketio(path):
+    socketio_manage(request.environ, {'': ChatNamespace})
+
+
 medical_details_service = MedicalDetailsService()
 member_service = MemberService()
 claim_service = ClaimService()
